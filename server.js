32653c08741fd26d949dace1caa807e023695277
patricia-dalinis <47152085+patricia-dalinis@users.noreply.github.com>
2019-08-05 01:33:01 -0500

    Attempting to fix Heroku

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ if (process.env.NODE_ENV === "production") {
 // Define API routes here
 
 const mongoose = require("mongoose");
-const mongoURL = process.env.PROD_MONGODB || "mongodb://localhost:27017/googlebooks"
+const mongoURL = process.env.PROD_MONGODB || "mongodb://heroku_f16xbjfm:password1@ds219672.mlab.com:19672/heroku_f16xbjfm"
 mongoose.connect(mongoURL, {useNewUrlParser: true})
   .then(() => {
     console.log("Successfully connected to mongoDB.");
